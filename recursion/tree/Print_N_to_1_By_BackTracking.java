8ad0419e6ae5246d2fd5ad3ef891140d5e024e10
subrato.sarkar <subrato.sarkar@lookout.com>
2022-11-25 13:08:17 -0500

    more code

--- recursion/tree/Print_N_to_1_By_BackTracking.java
+++ recursion/tree/Print_N_to_1_By_BackTracking.java
@@ -9,7 +9,7 @@ public class Print_N_to_1_By_BackTracking {
 
     public static void print (int iteration, int n) {
         if (iteration > n) {
-            return;
+            return; // end of function. Rest of lines will no more be executed.
         }
         print(iteration + 1, n);
         System.out.println(iteration); //printing after function call
